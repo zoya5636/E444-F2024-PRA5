ba17cd5b2be405cc9974d501489271e13cf975f2
zoya5636 <zoya@chishtie.com>
2024-10-24 19:52:17 -0400

    Using pytest benchmark and generating boxplot

--- tests/application_test.py
+++ tests/application_test.py
@@ -1,5 +1,61 @@
 from application import application
 import json
+from pytest_benchmark.plugin import benchmark
+import requests
+import time
+import pytest
+import csv
+
+test_file_name = "tests.csv"
+
+testing_news = {
+    "fake1": "This is fake news",
+    "fake2": "This is also fake news",
+    "real1": "Americans are casting their ballots on November 5 to elect the 47th president of the United States.",
+    "real2": "High grocery and rental costs are squeezing lower-income Canadians even as inflation(opens in a new tab) trends downward, a new survey suggests."
+}
+
+def hit_external_endpoint(news, writer):
+    request_sent_time = time.time()
+    response = requests.get(f'http://serve-sentiment4-env.eba-xrfx4vua.us-east-2.elasticbeanstalk.com/?news={news}')
+    response_recieved_time = time.time()
+    writer.writerow([news, response_recieved_time - request_sent_time, json.loads(response.text)])
+
+@pytest.mark.benchmark(
+    min_rounds=100
+)
+def test_for_fake_news_1_100_runs(benchmark):
+    with open('test-fake-1.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Testcase", "Time", "Response"])
+        benchmark(hit_external_endpoint, testing_news["fake1"], writer)
+
+@pytest.mark.benchmark(
+    min_rounds=100
+)
+def test_for_fake_news_2_100_runs(benchmark):
+    with open('test-fake-2.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Testcase", "Time", "Response"])
+        benchmark(hit_external_endpoint, testing_news["fake2"], writer)
+
+@pytest.mark.benchmark(
+    min_rounds=100
+)
+def test_for_real_news_1_100_runs(benchmark):
+    with open('test-real-1.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Testcase", "Time", "Response"])
+        benchmark(hit_external_endpoint, testing_news["real1"], writer)
+
+@pytest.mark.benchmark(
+    min_rounds=100
+)
+def test_for_real_news_2_100_runs(benchmark):
+    with open('test-real-2.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Testcase", "Time", "Response"])
+        benchmark(hit_external_endpoint, testing_news["real2"], writer)
 
 def test_for_fake_news_1():
     tester = application.test_client()
